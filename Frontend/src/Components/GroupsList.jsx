4691507fb5cc92308c4c80bd9b2858a21467b6b4
Shivansh Kush <kushivansh27@gmail.com>
2024-10-20 01:58:00 +0530

    added delete group

--- Frontend/src/Components/GroupsList.jsx
+++ Frontend/src/Components/GroupsList.jsx
@@ -6,20 +6,33 @@ const GroupsList = () => {
     const [isUpdateGroupVisible, setIsUpdateGroupVisible] = useState(false);
     const [currentSelectedGroupName, setCurrentSelectedGroupName] = useState(null);
 
-    useEffect(() => {
-        const fetchData  = async() => {
-            let response = await fetch("https://youbettersplit.onrender.com/fetchGroupDetails");
-            let res = await response.json();
-            if(res && Array.isArray(res.data)){
-                setGroupName(res.data.map(g => g.groupName));
-            }
+    const fetchData  = async() => {
+        let response = await fetch("https://youbettersplit.onrender.com/fetchGroupDetails");
+        let res = await response.json();
+        if(res && Array.isArray(res.data)){
+            setGroupName(res.data.map(g => g.groupName));
         }
+    }
+
+    useEffect(() => {
         fetchData();
     }, [])
 
     function openUpdateGroup(e){
         setCurrentSelectedGroupName(e.target.parentElement.name)
         setIsUpdateGroupVisible(true);
+    }
+
+    const deleteGroup = async(e) => {
+        const groupName = e.target.parentElement.name;
+            await fetch("https://youbettersplit.onrender.com/deleteGroup",{
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({groupName})
+            })
+        fetchData();
     }
 
     function closeUpdateGroup(){
@@ -31,7 +44,10 @@ const GroupsList = () => {
             {groupName.map((name, index) => (
                 <div className="border-2 bg-sky-300 w-full p-10 flex justify-between mt-2" key={index}>
                 <h2 className="text-2xl">{name}</h2>
-                <button name={name} onClick={(e) => openUpdateGroup(e)}><i className="fa-solid fa-pen-to-square"></i></button>
+                <div>
+                <button name={name} onClick={(e) => openUpdateGroup(e)}><i className="mx-3 fa-solid fa-pen-to-square"></i></button>
+                <button name={name} onClick={(e) => deleteGroup(e)}><i className="mx-3 fa-solid fa-trash"></i></button>
+                </div>
                 </div>
             ))}
             <div  className="">
